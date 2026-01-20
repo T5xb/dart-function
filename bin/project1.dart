0d38a4d5bfa9c77714b86c82d23bfe997d3f46b1
Alwaleed <wlle-202@hotmail.com>
2026-01-20 20:20:13 +0300

    new object

--- bin/project1.dart
+++ bin/project1.dart
@@ -1,10 +1,14 @@
-
 void main() {
-  print('Hello world');
+  Person p = Person(name: "alwaleed", age: 20);
+  p.printName();
 }
 
 class Person {
   String? name;
   int? age;
   Person({name, age});
+
+  void printName() {
+    print("alwaleed");
+  }
 }
